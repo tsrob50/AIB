0689b9d1e686cfa1d37ba1d202c749f6834ab444
Travis Roberts <27257343+tsrob50@users.noreply.github.com>
2024-04-03 07:01:27 -0500

    Update winget.ps1
    Update with full path and machine scope

--- winget.ps1
+++ winget.ps1
@@ -3,7 +3,7 @@ Add-AppPackage -path "https://cdn.winget.microsoft.com/cache/source.msix."
 winget upgrade --accept-source-agreements
 
 # Install Foxit
-winget install Foxit.FoxitReader --silent
+C:\"Program Files"\WindowsApps\Microsoft.DesktopAppInstaller_1.17.10691.0_x64__8wekyb3d8bbwe\winget.exe install --scope machine Foxit.FoxitReader --silent
 
 # Install 7Zip
-winget install 7zip.7zip --silent
+C:\"Program Files"\WindowsApps\Microsoft.DesktopAppInstaller_1.17.10691.0_x64__8wekyb3d8bbwe\winget.exe install --scope machine 7zip.7zip --silent
