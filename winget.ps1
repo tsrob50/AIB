ac2b4c2a11b9f917b9641af900a8db8ecdf5cd08
Travis Roberts <27257343+tsrob50@users.noreply.github.com>
2024-04-03 07:06:05 -0500

    Update winget.ps1
    Add path to terms

--- winget.ps1
+++ winget.ps1
@@ -1,6 +1,6 @@
 # Command to terms
 Add-AppPackage -path "https://cdn.winget.microsoft.com/cache/source.msix."
-winget upgrade --accept-source-agreements
+C:\"Program Files"\WindowsApps\Microsoft.DesktopAppInstaller_1.17.10691.0_x64__8wekyb3d8bbwe\winget.exe upgrade --accept-source-agreements
 
 # Install Foxit
 C:\"Program Files"\WindowsApps\Microsoft.DesktopAppInstaller_1.17.10691.0_x64__8wekyb3d8bbwe\winget.exe install --scope machine Foxit.FoxitReader --silent
