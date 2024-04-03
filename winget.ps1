803b9101d3277a883912b05dc42d8ccf29e4c975
Travis Roberts <27257343+tsrob50@users.noreply.github.com>
2024-04-03 18:59:36 -0500

    Delete winget.ps1
    Winget issues with CIT

--- winget.ps1
+++
@@ -1,9 +0,0 @@
-# Command to terms
-Add-AppPackage -path "https://cdn.winget.microsoft.com/cache/source.msix."
-C:\"Program Files"\WindowsApps\Microsoft.DesktopAppInstaller_1.17.10691.0_x64__8wekyb3d8bbwe\winget.exe upgrade --accept-source-agreements
-
-# Install Foxit
-C:\"Program Files"\WindowsApps\Microsoft.DesktopAppInstaller_1.17.10691.0_x64__8wekyb3d8bbwe\winget.exe install --scope machine Foxit.FoxitReader --silent
-
-# Install 7Zip
-C:\"Program Files"\WindowsApps\Microsoft.DesktopAppInstaller_1.17.10691.0_x64__8wekyb3d8bbwe\winget.exe install --scope machine 7zip.7zip --silent
